71047a5a39838d9652158e9f8cca4bfca476a658
Stephen Freudenthaler <31257998+sfreudenthaler@users.noreply.github.com>
2021-04-22 20:06:35 -0400

    Create main.js
    Untested PoC

---
+++ main.js
@@ -0,0 +1,14 @@
+const newBaby = async(urgent) => {
+    family++
+    var onLeave = true
+    if (urgent == true) {
+        message = "Please contact richard.langlois@rbc.com"
+    }
+    else{
+        message = "I'll read and respond to your email upon my return in June."
+    }
+    return message
+}
+
+newBaby(true).then(console.log)
+//TODO: add shell interface so it asks if urgent and responds accordingly if saved and run locally
