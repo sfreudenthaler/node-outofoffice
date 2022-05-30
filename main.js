98e4bc2f46bccfdfd4aacebd47557b038abbfb9c
Neil Chudleigh <nchudleigh@users.noreply.github.com>
2022-05-30 14:09:43 -0400

    Falsy check is enough, richard can handle it.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 var family = 3;
 const newBaby = (urgent) => {
-    if (urgent == true) {
+    if (urgent) {
         message = "Please contact richard.langlois@rbc.com"
     }
     else{
-        message = "I'll read and respond to your email upon my return in June. I am exanding my family capacity from ${family} to ${family + 1}"
+        message = `I'll read and respond to your email upon my return in June. I am exanding my family capacity from ${family} to ${family + 1}`
     }
     return message
 }
