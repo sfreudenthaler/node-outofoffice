1780152751c855bf5f756e7c7a6118811634f93c
Neil Chudleigh <nchudleigh@users.noreply.github.com>
2022-05-30 14:12:37 -0400

    Since you hate semicolons, lets compromise and just add 2

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 
 var family = 3;
 const newBaby = (urgent) => {
-    if (urgent) {
-        message = "Please contact richard.langlois@rbc.com"
-    }
-    else{
-        message = `I'll read and respond to your email upon my return in June. I am exanding my family capacity from ${family} to ${family + 1}`
-    }
-    return message
+    return urgent ? 
+        "Please contact richard.langlois@rbc.com"
+        :`I'll read and respond to your email upon my return in June. I am exanding my family capacity from ${family} to ${family + 1}`;
 }
 
 
-console.log(newBaby(true))
+console.log(newBaby(true));
 //TODO: add shell interface so it asks if urgent and responds accordingly if saved and run locally
